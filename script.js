71d50137b4bcc75a2a379173b8a01570174dc247
B074n-debug <lenamilebonolo67@gmail.com>
2026-05-24 21:00:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Auto Clean Premium Detailing Botswana website loaded successfully");
+
+// Simple welcome message
+alert("Welcome to Auto Clean Premium Detailing Botswana !");
+
+// Function for booking confirmation
+function confirmBooking() {
+    alert("Thank you very much! Your booking has been submitted. We hope to see you again");
+}
